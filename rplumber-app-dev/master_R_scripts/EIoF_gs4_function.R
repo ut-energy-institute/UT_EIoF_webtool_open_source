cade720bbd71f96d387a279d07858e7eff7e21fb
King <careyking@energy.utexas.edu>
2020-05-18 13:16:51 -0500

    reduced comments for "gargle" package accessing Google Sheets

--- rplumber-app-dev/master_R_scripts/EIoF_gs4_function.R
+++ rplumber-app-dev/master_R_scripts/EIoF_gs4_function.R
@@ -143,7 +143,7 @@ EIoF_gs4_function <- function(SG_out){
   ## +++++++++++++
   googlesheet_fileID = "16jQuodzL_jBE6lOgbVvahovt7mqDq-9a47Ndt97KAA0"
   gargle::token_fetch()
-  options(gargle_quiet = FALSE)
+  #options(gargle_quiet = FALSE)
   gs4_auth(email = "eiof-project@ambient-segment-274318.iam.gserviceaccount.com",
            scopes = "https://www.googleapis.com/auth/spreadsheets",
            path = "sa.json")
